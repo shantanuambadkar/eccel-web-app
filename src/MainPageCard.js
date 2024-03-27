696eedb4e472b7327e1785ec9e85cd926fab1056
Shantanu Ambadkar <89132314+shantanuambadkar@users.noreply.github.com>
2024-03-27 22:36:09 +0530

    removed warnings

--- src/MainPageCard.js
+++ src/MainPageCard.js
@@ -1,4 +1,4 @@
-import { Card, CardContent, Typography } from '@mui/material';
+import { Card, CardContent } from '@mui/material';
 import './MainPageCard.css';
 import { useNavigate } from 'react-router-dom';
 
