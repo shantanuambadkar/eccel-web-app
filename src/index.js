485b4530ef504dfc6995994bb6184ce91ba9a01d
Shantanu A <shantanuambadkar@gmail.com>
2024-04-19 05:32:14 +0530

    added lazy loader

--- src/index.js
+++ src/index.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, {lazy, Suspense} from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MainPage from './MainPage';
+const MainPage = lazy(() => import('./MainPage'));
+const AppPage = lazy(() => import('./App'));
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
+    <Suspense fallback={<div>Loading...</div>}>
       <Routes>
         <Route path="/" element={<MainPage />} />
-        <Route path="/rvsf" element={<App />} />
+        <Route path="/rvsf" element={<AppPage />} />
       </Routes>
+      </Suspense>
     </Router>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
